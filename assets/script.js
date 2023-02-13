c9da3f7c6d6546b0c9d8f257e953076163d26625
shane davis <120618274+Basil-NB@users.noreply.github.com>
2023-02-12 22:36:15 -0500

    added if statements for all of the prompts and added a password variable with a for loop that randomly calculates the password based on the user imputs in the prompts

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,35 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 function generatePassword() {
-  var prompt1 = window.prompt("How many characters does your password need?")
+  var prompt1 = parseInt(prompt("How many characters does your password need?"))
   var prompt2 = confirm("Select OK to add special characters.")
   var prompt3 = confirm("select OK to add numbers.")
   var prompt4 = confirm("select OK to add lowercase letters.")
   var prompt5 = confirm("select OK to add uppercase letters.")
 
+  var passwordOptions = "";
+  if (prompt2) {
+    passwordOptions += "!@#$%^&*()_+-=[]{}|;':,.<>/?\\";
+  }
+  if (prompt3) {
+    passwordOptions += "0123456789";
+  }
+  if (prompt4) {
+    passwordOptions += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (prompt5) {
+    passwordOptions += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
 
+  var password = "";
+  for (var i = 0; i < prompt1; i++) {
+    password += passwordOptions.charAt(Math.floor(Math.random() * passwordOptions.length));
+  }
 
-}
+  return("Your new password is: " + password);
+  
+} 
+
 
 function writePassword() {
   var password = generatePassword();
