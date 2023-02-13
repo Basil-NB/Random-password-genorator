8c428ffca0ba9e7d12f26144adca83a9331e0e4a
shane davis <120618274+Basil-NB@users.noreply.github.com>
2023-02-12 21:35:34 -0500

    added function for generatepassword and added window prompt variables inside

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,18 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+function generatePassword() {
+  var prompt1 = window.prompt("How many characters does your password need?")
+  var prompt2 = confirm("Select OK to add special characters.")
+  var prompt3 = confirm("select OK to add numbers.")
+  var prompt4 = confirm("select OK to add lowercase letters.")
+  var prompt5 = confirm("select OK to add uppercase letters.")
+
+
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
